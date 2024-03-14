3dfcc539851d6bf0958edbfb83ef47af15e622ec
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-03-14 14:50:15 +0200

    fix store

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; 
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import './index.css';
+import App from './App.jsx';
+import store, { persistedStore } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/capybara-components-frontend">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    <Provider store={store}>
+      <PersistGate persistor={persistedStore}>
+        <BrowserRouter basename="/capybara-components-frontend">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
