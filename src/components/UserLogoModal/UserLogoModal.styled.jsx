b6b1adbdce0152e95a750b144062a9ae3cfd0850
SVTroya <my.svtroy@gmail.com>
2024-03-14 17:38:12 +0200

    PopUp menu with Settings and Log Out buttons

---
+++ src/components/UserLogoModal/UserLogoModal.styled.jsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const MenuContainer = styled.div`
+  border-radius: 10px;
+  padding: 16px;
+  width: 118px;
+  height: 88px;
+  box-shadow: 0 4px 8px 0 rgba(64, 123, 255, 0.2);
+  background-color: var(--white);
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const MenuButton = styled.button`
+  background-color: transparent;
+  border: none;
+  padding: 0;
+  display: flex;
+  gap: 8px;
+
+  font-size: 16px;
+  line-height: 1.25;
+  color: var(--blue);
+  
+  transition: color 250ms ease-in-out;
+  
+  &:hover, &:focus{
+    color: var(--secondary-orange);
+  }
+  
+  svg{
+    fill: currentColor;
+  }
+  
+  
+`;
