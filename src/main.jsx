86887e8f08e4371619da35ba5448d68e570e524a
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-03-14 12:11:25 +0200

    Revert "Add to src"

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import App from './App.jsx';
+import App from './App.jsx'; 
 import './index.css';
-import store, { persistedStore } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistedStore}>
-        <BrowserRouter basename="/capybara-components-frontend">
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+    <BrowserRouter basename="/capybara-components-frontend">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
