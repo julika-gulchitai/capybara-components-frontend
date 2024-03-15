2285f3a4705685f0b3f5ecef15bf07a5443b45c4
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-03-15 11:51:26 +0200

    Update VariablesJSX.jsx

--- src/css/VariablesJSX.jsx
+++ src/css/VariablesJSX.jsx
@@ -1,18 +1,16 @@
 const theme = Object.freeze({
-    colors: {
-      primaryLight: '#ffffff',
-      primaryDark: '#2F2F2F',
-      primaryAccent: '#407BFF',
-  
-      secondaryLight: '#ECF2FF',
-      secondaryLightBlue: '#D7E3FF',
-      secondaryBlue: '#9EBBFF',
-      secondaryRed: '#EF5050',
-      secondaryYellow: '#FF9D43',
-      secondaryGray: '#8F8F8F',
-    },
-  
-  });
-  
-  export default theme;
-  
+  colors: {
+    white: '#ffffff',
+    black: '#2F2F2F',
+    blue: '#407BFF',
+
+    secondaryWhite: '#ECF2FF',
+    secondaryLightblue: '#D7E3FF',
+    secondaryBlue: '#9EBBFF',
+    secondaryRed: '#EF5050',
+    secondaryOrange: '#FF9D43',
+    secondaryGray: '#8F8F8F',
+  },
+});
+
+export default theme;
