1d472d3f03c32f72ea343ca0b749051ec1dcd10c
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-03-15 17:03:47 +0200

    + selector to water

--- src/redux/Water/selectors.js
+++ src/redux/Water/selectors.js
@@ -1,3 +1,2 @@
-export const selectIsLogged = (state) => state.user.isLoggIn;
 export const selectToken = (state) => state.user.token;
 export const selectEntries = (state) => state.tracker.entries;
