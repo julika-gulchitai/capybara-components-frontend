f92d909ed9ef61fff27b047fc2c5f489e146af90
Yulia Palamarchuk <83129717+YuliaMiha@users.noreply.github.com>
2024-03-17 09:35:20 +0200

    update sectionlistwater

--- src/components/SectionWaterList/SectionWaterList.styled.js
+++ src/components/SectionWaterList/SectionWaterList.styled.js
@@ -1,21 +1,25 @@
 import styled from 'styled-components';
 
 export const StyledSectionWaterList = styled.div`
-  margin-bottom: 40px;
-  padding: 24px 8px;
+  padding: 32px 24px;
   display: flex;
   flex-direction: column;
-  gap: 24px;
+  gap: 209px;
+
+  width: 592px;
+  height: 680px;
+
+  margin-left: 736px;
+  margin-top: calc(-80px + 0px);
 
   border-radius: 10px;
-  background: var(--secondary-lightblue);
+  background: var(--secondary-white);
   box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.3);
 
   @media screen and (min-width: 768px) {
     margin-bottom: 44px;
-    padding: 32px 24px;
-    height: 688px;
   }
+
   @media only screen and (min-width: 1440px) {
     height: 680px;
   }
