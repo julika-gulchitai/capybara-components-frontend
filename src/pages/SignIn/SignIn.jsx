92a7256e26a9dff39e749782e9ddebbfdb1ee189
Uliana Nadorozhna <102276922+UlianaNad@users.noreply.github.com>
2024-03-18 11:04:18 +0200

    added verification message for user

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -2,12 +2,29 @@ import AuthForm from '../../components/AuthForm/AuthForm';
 
 import { MarginBetween, StyledNavlink, Title, WraperForm, Wrapper } from '../SignUp/SignUp.styled';
 import BottleComponent from '../../components/AuthForm/BottleComponent/BottleComponent';
+import { Notify } from 'notiflix';
 
 
 
 
 const SignIn = () => {
 
+const params = new URLSearchParams(window.location.search);
+const message = params.get('message');
+console.log(message)
+if (message) {
+  Notify.success(
+    'You have activated your account. Now you can login.',
+    {
+      timeout: 5000,
+      width: '460px',
+      height:'200px',
+      messageMaxLength:500,
+      fontSize:"14px",
+      position:"center-top",
+    }
+  ); 
+}
 
   return (
     <Wrapper>
