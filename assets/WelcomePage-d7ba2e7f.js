e6f5154d1d3d483a4eecd275c93a385c61ffb136
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-03-19 14:50:22 +0000

    Deploying to gh-pages from @ julika-gulchitai/capybara-components-frontend@98f601bcbba58f9d919269bf48d786a467ed59ea 🚀

--- assets/WelcomePage-d7ba2e7f.js
+++ assets/WelcomePage-d7ba2e7f.js
@@ -1,18 +1,18 @@
-import{u as i,b as a,A as s,L as d,j as e,T as o,s as t}from"./index-620def67.js";const l="/capybara-components-frontend/assets/background-main-page-layers-5141a367.svg",c="/capybara-components-frontend/assets/background-welcome-page-e568c387.svg",m="/capybara-components-frontend/assets/background-main-page-f-382561be.svg",h=i.div`
+import{u as i,b as r,A as s,L as o,a as l,j as n,T as d,s as t,t as c}from"./index-68836ba5.js";const m="/capybara-components-frontend/assets/background-main-page-layers-5141a367.svg",h="/capybara-components-frontend/assets/background-welcome-page-e568c387.svg",g="/capybara-components-frontend/assets/background-main-page-f-382561be.svg",p=i.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color:${n=>n.theme.colors.white};
+  background-color:${e=>e.theme.colors.white};
   background-repeat: no-repeat;
   background-position: bottom;
   @media only screen and (max-width: 320px){
-    background-image: url(${c});
+    background-image: url(${h});
   }
   @media (min-width:321px) and (max-width: 768px){
-    background-image:url(${m});
+    background-image:url(${g});
   }
   @media (min-width:769px) {
-    background-image: url(${l}), url(${a});    
+    background-image: url(${m}), url(${r});    
   }
 
   @media only screen and (min-width: 768px) {
@@ -25,13 +25,13 @@ import{u as i,b as a,A as s,L as d,j as e,T as o,s as t}from"./index-620def67.js
       align-items: flex-start;
       justify-content: center;
   }
-`,p=i(s)`
+`,x=i(s)`
   display: flex;
   flex-direction: column;
   @media only screen and (min-width: 1440px) {
     flex-direction: row;
     }
-`,x=i.div`
+`,w=i.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -46,7 +46,7 @@ import{u as i,b as a,A as s,L as d,j as e,T as o,s as t}from"./index-620def67.js
     margin: 4.2rem 5.5rem 4.2rem 6.5rem;
     gap: 5rem;
   }
-`,g=i.div`
+`,y=i.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
@@ -62,7 +62,7 @@ h3{
     margin-bottom: 1rem;
   }
   }
-`,y=i.h1`
+`,f=i.h1`
  font-size: 1.75rem;
 margin: 0;
   @media only screen and (min-width: 768px) {
@@ -72,14 +72,14 @@ margin: 0;
   @media only screen and (min-width: 1440px) {
     line-height: 1.16667;
   }
-`,f=i.h2`
+`,u=i.h2`
   font-weight: 400;
   line-height: 1.25;
   margin: 1rem 0;
   @media only screen and (min-width: 768px) {
     letter-spacing: 1px;
   }
-`,u=i.ul`
+`,b=i.ul`
   margin: 0;
   padding: 0;
   text-align: left;
@@ -105,17 +105,17 @@ margin: 0;
       margin-bottom: 1rem;
     }
   }
-`,r=i.svg`
-  width: ${n=>n.size||"32px"};
-  height: ${n=>n.size||"32px"};
-  fill: ${n=>n.color||"#fffff"};
+`,a=i.svg`
+  width: ${e=>e.size||"32px"};
+  height: ${e=>e.size||"32px"};
+  fill: ${e=>e.color||"#fffff"};
   display: inline - block;
   margin-right: 5px;
   @media only screen and (min-width: 768px) {
-    width: ${n=>n.size||"40px"};
-    height: ${n=>n.size||"40px"};
+    width: ${e=>e.size||"40px"};
+    height: ${e=>e.size||"40px"};
   }
-`,w=i.div`
+`,j=i.div`
   border-radius: 10px;
   box-shadow: 0 4px 14px 0 rgba(64, 123, 255, 0.3);
   background: var(--secondary-white);
@@ -125,13 +125,13 @@ margin: 0;
   @media only screen and (min-width: 768px) {
     margin-top: 1.25rem;
   }
-  `,b=i.h3`
+  `,k=i.h3`
   font-size: 18px;
   line-height: 1.11111;
   margin: 0 -1rem;
   @media only screen and (min-width: 1440px) {
     margin-top: .5rem;
-  }`,j=i.ul`
+  }`,P=i.ul`
   padding: 1.5rem 1rem 1.5rem 2rem;
   display: flex;
   flex-direction: column;
@@ -155,19 +155,19 @@ margin: 0;
       margin-right: 6px;
     }
   }
-`,k=i(d)`
+`,v=i(o)`
   border-radius: 10px;
   border: none;
   padding: .5rem;
   margin-top: 1.5rem;
   margin-bottom: 2.5rem;
   box-shadow: 0 4px 8px 0 rgba(64, 123, 255, 0.34);
-  background: ${n=>n.theme.colors.blue};
+  background: ${e=>e.theme.colors.blue};
   font-weight: 500;
   font-size: 18px;
   line-height: 1.33333;
   text-align: center;
-  color: ${n=>n.theme.colors.white};
+  color: ${e=>e.theme.colors.white};
   width: 100%;
   @media only screen and (min-width: 768px) {
     width: 50%;
@@ -176,4 +176,4 @@ margin: 0;
     width: 24rem;
     margin-top: .5rem;
   }
-`,$=()=>e.jsx(o,{children:e.jsx(h,{children:e.jsx(p,{children:e.jsxs(x,{children:[e.jsxs(g,{children:[e.jsx(y,{children:"Water consumption tracker"}),e.jsx(f,{children:"Record daily water intake and track"}),e.jsx("h3",{children:"Tracker Benefits"}),e.jsxs(u,{children:[e.jsxs("li",{children:[" ",e.jsx(r,{children:e.jsx("use",{href:`${t}#calendar`})}),"Habit drive"]}),e.jsxs("li",{children:[" ",e.jsx(r,{children:e.jsx("use",{href:`${t}#presentation-chart-bar`})}),"View statistics"]}),e.jsxs("li",{children:[" ",e.jsx(r,{children:e.jsx("use",{href:`${t}#wrench-schewdriver`})}),"Personal rate setting"]})]}),e.jsx(k,{to:"/signup",children:"Try tracker"})]}),e.jsx(w,{children:e.jsxs(j,{children:[e.jsx(b,{children:"Why drink water"}),e.jsx("li",{children:"Supply of nutrients to all organs"}),e.jsx("li",{children:"Providing oxygen to the lungs"}),e.jsx("li",{children:e.jsx("span",{children:"Maintaining the work of the heart"})}),e.jsx("li",{children:"Release of processed substances"}),e.jsx("li",{children:"Ensuring the stability of the internal environment"}),e.jsx("li",{children:"Maintaining within the normal temperature"}),e.jsx("li",{children:"Maintaining an immune system capable of resisting disease"})]})})]})})})});export{$ as default};
+`,S=()=>{const{t:e}=l();return n.jsx(d,{children:n.jsx(p,{children:n.jsx(x,{children:n.jsxs(w,{children:[n.jsxs(y,{children:[n.jsx(f,{children:e("welcomePage.Water consumption tracker")}),n.jsx(u,{children:e("welcomePage.Record daily water intake and track")}),n.jsx("h3",{children:e("welcomePage.Tracker Benefits")}),n.jsxs(b,{children:[n.jsxs("li",{children:[" ",n.jsx(a,{children:n.jsx("use",{href:`${t}#calendar`})}),e("welcomePage.Habit drive")]}),n.jsxs("li",{children:[" ",n.jsx(a,{children:n.jsx("use",{href:`${t}#presentation-chart-bar`})}),e("welcomePage.View statistics")]}),n.jsxs("li",{children:[" ",n.jsx(a,{children:n.jsx("use",{href:`${t}#wrench-schewdriver`})}),e("welcomePage.Personal rate setting")]})]}),n.jsx(v,{theme:c,to:"/signup",children:e("welcomePage.Try tracker")})]}),n.jsx(j,{children:n.jsxs(P,{children:[n.jsx(k,{children:e("welcomePage.Why drink water")}),n.jsx("li",{children:e("welcomePage.Supply of nutrients to all organs")}),n.jsx("li",{children:e("welcomePage.Providing oxygen to the lungs")}),n.jsx("li",{children:n.jsx("span",{children:e("welcomePage.Maintaining the work of the heart")})}),n.jsx("li",{children:e("welcomePage.Release of processed substances")}),n.jsx("li",{children:e("welcomePage.Ensuring the stability of the internal environment")}),n.jsx("li",{children:e("welcomePage.Maintaining within the normal temperature")}),n.jsx("li",{children:e("welcomePage.Maintaining an immune system capable of resisting disease")})]})})]})})})})};export{S as default};
