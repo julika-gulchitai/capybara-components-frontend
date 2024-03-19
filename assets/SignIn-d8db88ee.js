6b22bda294877d3f9a08dfb358d02e41e42122c6
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-03-19 10:38:38 +0000

    Deploying to gh-pages from @ julika-gulchitai/capybara-components-frontend@58c68226b877b5535a45b97ced7f25dfca8df4d3 🚀

---
+++ assets/SignIn-d8db88ee.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-5d7514aa.js";import{W as r,a as n,T as e,A as t,M as a,S as i,B as o}from"./BottleComponent-fb47ccd2.js";const j=()=>s.jsxs(r,{children:[s.jsxs(n,{children:[s.jsx(e,{children:"Sign In"}),s.jsx(t,{}),s.jsx(a,{}),s.jsx(i,{to:"/signup",children:"Sign Up"})]}),s.jsx(o,{})]});export{j as default};
