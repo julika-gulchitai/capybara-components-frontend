0e4a825b24a77c240e224f2194a7367aacabf384
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-03-19 12:05:24 +0000

    Deploying to gh-pages from @ julika-gulchitai/capybara-components-frontend@618ac77d5c5e3102ef95cb0efc55e4740a149981 🚀

---
+++ assets/AuthForm.styled-89a1eb3a.js
@@ -0,0 +1,33 @@
+import{u as i,B as t}from"./index-cc7623e4.js";const p=i.div`
+  display: block;
+  margin: 0 auto;
+
+`,a=i.div`
+  font-weight: 400;
+  font-size: 18px;
+  margin-bottom: 8px;
+  line-height: 1.33333;
+
+  color: var(--black);
+`,o=i(t)`
+  font-size: 16px;
+  width: 280px;
+  margin: 0;
+  line-height: 1.25;
+  padding: 8px 30px;
+  width: 280px;
+  height: 36px;
+  z-index: 100;
+  @media only screen and (min-width: 768px) {
+    font-size: 18px;
+    line-height: 1.33333;
+    padding: 10px 30px;
+    width: 336px;
+    height: 44px;
+  }
+  @media only screen and (min-width: 1440px) {
+    width: 384px;
+  }
+`,e=i.div`
+  margin-bottom: 6px;
+`;export{a as L,o as S,p as W,e as a};
