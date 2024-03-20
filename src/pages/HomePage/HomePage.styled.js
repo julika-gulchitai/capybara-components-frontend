0e20c58444feda0c224aa36533bb1e57b81a37ce
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-03-20 17:28:22 +0200

    bugs-fix

--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -1,39 +1,74 @@
 import styled from 'styled-components';
-import {AdaptiveContainer} from '../../components/Container/Container.styled.jsx';
+import { AdaptiveContainer } from '../../components/Container/Container.styled.jsx';
+import bgDesc from '../../assets/desktop/background-element-main-page.svg';
+import bgTab from '../../assets/tablet/background-element-home-screen.svg';
+import bgMob from '../../assets/mobile/background-element-home-screen.svg';
+import bottleDesc from '../../assets/desktop/botle-home-screen.svg';
+import bottleTab from '../../assets/tablet/bottle-home-screen.svg';
+import bottleMob from '../../assets/mobile/bottle-home-screen.svg';
 
 export const HomeContainer = styled(AdaptiveContainer)`
   display: flex;
   flex-direction: column;
   gap: 40px;
+  position: relative;
+  /* height: calc(100vh - 48px); */
+  background-image: url(${bgMob});
+  background-position: top;
+     padding-top: 16px;
+    padding-bottom: 82px; 
+  
+
+  @media only screen and (min-width: 768px) {
+    background-image: url(${bgTab});
+
+  }
 
   @media only screen and (min-width: 1440px) {
     flex-direction: row;
     gap: 32px;
-  }
+    background-position: bottom;
+    background-image: url(${bgDesc});
+    padding-bottom: 0; 
 `;
 
 export const LeftContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: url(${bottleMob});
+  height: 448px;
+  margin-bottom: 40px;
 
-  height: 441px;
-  
   @media only screen and (min-width: 768px) {
-    height: 492px;
+    height: 564px;
+    /* background-image: url(${bgTab}); */
+    background-position: top;
+    background-image: url(${bottleTab});
+    height: 481px;
+    background-position: top;
+    margin-bottom: 20px;
   }
-  
+
   @media only screen and (min-width: 1440px) {
     width: calc((100% / 2) - 16px);
-    height: 680px;
+    height: 690px;
     padding-top: 16px;
     padding-bottom: 32px;
+    background-position: top;
+    background-image: url(${bottleDesc});
+   
+  }
   }
 `;
 
 export const RightContainer = styled.div`
   @media only screen and (min-width: 1440px) {
-    width: calc((100% / 2) - 16px );
+    width: calc((100% / 2) - 16px);
     height: 680px;
+    position: absolute;
+    right: -10px;
   }
 `;
