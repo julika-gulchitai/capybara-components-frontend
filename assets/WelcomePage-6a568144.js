0b8b7185f4a23d094f00a7a7b91588d185db1bd3
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-03-21 17:36:46 +0000

    Deploying to gh-pages from @ julika-gulchitai/capybara-components-frontend@3264595708625bc3b4b325cd62aa81957543a2ab 🚀

--- assets/WelcomePage-6a568144.js
+++ assets/WelcomePage-6a568144.js
@@ -1,4 +1,4 @@
-import{u as t,A as s,L as d,a as m,j as n,s as a}from"./index-ece983ef.js";const c=t.div`
+import{u as t,A as s,L as d,a as m,j as n,s as a}from"./index-dd8c6f28.js";const c=t.div`
   display: flex;
   align-items: center;
   justify-content: center;
