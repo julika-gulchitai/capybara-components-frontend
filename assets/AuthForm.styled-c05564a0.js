2d6459153078d988a6fd891fda8b7d5b2cef8b1e
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-03-21 18:58:08 +0000

    Deploying to gh-pages from @ julika-gulchitai/capybara-components-frontend@10f1776115b652e44322d85f8eb66c72ea6d27fd 🚀

--- assets/AuthForm.styled-c05564a0.js
+++ assets/AuthForm.styled-c05564a0.js
@@ -1,4 +1,4 @@
-import{u as t,B as n}from"./index-9141ca7a.js";const e=t.div`
+import{u as t,B as n}from"./index-ccc02e18.js";const e=t.div`
   display: block;
   margin: 0 auto;
 
--- assets/WelcomePage-cda62956.js
+++ assets/WelcomePage-cda62956.js
@@ -1,4 +1,4 @@
-import{u as t,A as s,L as d,a as m,j as n,s as a}from"./index-9141ca7a.js";const c=t.div`
+import{u as t,A as s,L as d,a as m,j as n,s as a}from"./index-ccc02e18.js";const c=t.div`
   display: flex;
   align-items: center;
   justify-content: center;
