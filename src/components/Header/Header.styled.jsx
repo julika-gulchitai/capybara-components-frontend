6965d92920f3c77ac77486af486cb9370c7b8eb9
SVTroya <my.svtroy@gmail.com>
2024-03-22 12:46:17 +0200

    Language and theme panel for mobile

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -24,3 +24,14 @@ export const SwitchesBlock = styled.div`
   width: fit-content;
 `;
 
+export const SwitchesFloatingBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  box-shadow: 0 4px 8px 0 rgba(158, 187, 255, 0.12);
+  background-color:${props => props.theme.colors.mainBg};
+  border: 1px solid var(--secondary-white);
+  border-radius: 10px;
+`;
