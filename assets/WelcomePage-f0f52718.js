3c76e671277a23c4e6e30c541d267396885e47ad
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-03-23 17:07:42 +0000

    Deploying to gh-pages from @ julika-gulchitai/capybara-components-frontend@e3be09b9efd7f23102bf258d320e6efe46afdf4f 🚀

--- assets/WelcomePage-f0f52718.js
+++ assets/WelcomePage-f0f52718.js
@@ -1,4 +1,4 @@
-import{u as i,A as r,L as o,a as l,j as n,s as t}from"./index-83bdc69f.js";const d=i.div`
+import{u as i,A as r,L as o,a as l,j as n,s as t}from"./index-a8358e8c.js";const d=i.div`
   background-color:${e=>e.theme.colors.mainBg};
   background-repeat: no-repeat;
   background-position: bottom;
