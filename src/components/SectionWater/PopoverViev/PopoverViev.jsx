4c6fff0668b15eeb2f2f682c3749238b9fbc0b4f
Yulia Palamarchuk <83129717+YuliaMiha@users.noreply.github.com>
2024-03-23 18:56:44 +0200

    update percent

--- src/components/SectionWater/PopoverViev/PopoverViev.jsx
+++ src/components/SectionWater/PopoverViev/PopoverViev.jsx
@@ -25,19 +25,22 @@ const PopoverViev = ({ item, index, monthName }) => {
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
 
+  const displayedPercentage = Math.min(100, waterVolumePercentage); // Обмежуємо відсотки до 100%
+
   return (
     <StyleWrapperDay>
       <StyledBtn
         aria-describedby={id}
         onClick={handleClick}
         style={{
-          border: waterVolumePercentage >= 100 ? 'none' : '1px solid',
-          borderColor: waterVolumePercentage >= 100 ? 'transparent' : '#ff9d43',
+          border: displayedPercentage >= 100 ? 'none' : '1px solid',
+          borderColor: displayedPercentage >= 100 ? 'transparent' : '#ff9d43',
         }}
       >
         {index + 1}
       </StyledBtn>
-      <Percentage>{waterVolumePercentage}%</Percentage>
+      <Percentage>{displayedPercentage}%</Percentage>{' '}
+      {/* Використовуємо обмежене значення відсотків */}
       <Popover
         id={id}
         open={open}
