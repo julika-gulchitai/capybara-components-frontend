2d80e283d6ce96d8cbead009d95e5e75bb5341bd
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-03-24 17:24:38 +0000

    Deploying to gh-pages from @ julika-gulchitai/capybara-components-frontend@463c72e46a05f5e9a3b6e527affa4886fc3527ac 🚀

--- assets/SignUp-a5cd3751.js
+++
@@ -1,1 +0,0 @@
-import{a,j as s,a4 as r,a5 as i,a6 as e,a7 as t,a8 as o,a9 as l}from"./index-fd16ae85.js";import{A as x}from"./AuthForm-bccd1c0b.js";const d=()=>{const{t:n}=a();return s.jsx(r,{children:s.jsx(i,{children:s.jsxs(e,{children:[s.jsx(t,{children:n("signup")}),s.jsx(x,{signUp:!0}),s.jsx(o,{}),s.jsx(l,{to:"/signin",children:n("signin")})]})})})};export{d as default};
---
+++ assets/SignUp-ff75b2c2.js
@@ -0,0 +1,1 @@
+import{a,j as s,a4 as r,a5 as i,a6 as e,a7 as t,a8 as o,a9 as l}from"./index-517529e4.js";import{A as x}from"./AuthForm-f46e3516.js";const d=()=>{const{t:n}=a();return s.jsx(r,{children:s.jsx(i,{children:s.jsxs(e,{children:[s.jsx(t,{children:n("signup")}),s.jsx(x,{signUp:!0}),s.jsx(o,{}),s.jsx(l,{to:"/signin",children:n("signin")})]})})})};export{d as default};
