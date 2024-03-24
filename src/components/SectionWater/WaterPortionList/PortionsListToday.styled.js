9a0c48a84c0eb2afe889cdff742da5cb987daefa
SVTroya <my.svtroy@gmail.com>
2024-03-24 15:43:38 +0200

    Edit/Delete buttons fixed

--- src/components/SectionWater/WaterPortionList/PortionsListToday.styled.js
+++ src/components/SectionWater/WaterPortionList/PortionsListToday.styled.js
@@ -107,14 +107,30 @@ export const Button = styled.button`
   padding: 0;
   border: none;
   background-color: transparent;
+  position: relative;
+  color: ${props => props.theme.colors.secondaryBlue};
 
-  &:hover::after {
+  &::after {
+    content: '';
+    height: 1px;
+    width: 0;
+    position: absolute;
+    bottom: 1px;
+    left: 0;
+    background-color: currentColor;
+
+    transition: width 250ms ease-in-out;
+  }
+
+  &:hover::after,
+  &:focus::after {
     width: 16px;
   }
 
   & svg {
     width: 16px;
     height: 16px;
+    fill: currentColor;
   }
 `;
 
