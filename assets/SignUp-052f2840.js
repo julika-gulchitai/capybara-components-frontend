e52bb790fb836ea5a49a1b34d40562648253fa86
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-03-25 21:44:33 +0000

    Deploying to gh-pages from @ julika-gulchitai/capybara-components-frontend@dba9fa969013be978e077d347dd2317e31b5ff65 🚀

--- assets/SignUp-052f2840.js
+++
@@ -1,1 +0,0 @@
-import{a as n,j as s,a8 as r,a9 as i,aa as e,ab as t,ac as o,ad as l}from"./index-7f47ec52.js";import{A as x}from"./AuthForm-8927ef01.js";const j=()=>{const{t:a}=n();return s.jsx(r,{children:s.jsx(i,{children:s.jsxs(e,{children:[s.jsx(t,{children:a("signup")}),s.jsx(x,{signUp:!0}),s.jsx(o,{}),s.jsx(l,{to:"/signin",children:a("signin")})]})})})};export{j as default};
