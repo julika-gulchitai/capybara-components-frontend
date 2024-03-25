c7146e412b7beb8befa2566e47aa39f392a58796
MarianaSko <maryana.skorynovych@gmail.com>
2024-03-25 15:27:06 +0200

    width of DailyNorma component fixed

--- src/components/DailyNorma/DailyNorma.styled.jsx
+++ src/components/DailyNorma/DailyNorma.styled.jsx
@@ -1,18 +1,24 @@
 import styled from 'styled-components';
 
+export const ContainerWidthUk = styled.div`
+  width: 198px;
+`;
+export const ContainerWidthEn = styled.div`
+  width: 164px;
+`;
+
 export const DailyNormaContainer = styled.div`
-  box-shadow: ${props => props.theme.shadows.secondaryShadowContainer};
-  background-color:${props => props.theme.colors.mainBg};
+  box-shadow: ${(props) => props.theme.shadows.secondaryShadowContainer};
+  background-color: ${(props) => props.theme.colors.mainBg};
   border: 1px solid ${(props) => props.theme.colors.secondaryBg};
   border-radius: 10px;
   padding: 8px 20px;
-  width: 164px;
 
   h2 {
     font-weight: 500;
     font-size: 18px;
     line-height: 1.33;
-    color: ${props => props.theme.colors.textColor};
+    color: ${(props) => props.theme.colors.textColor};
     min-width: 124px;
     margin-bottom: 12px;
   }
@@ -21,7 +27,7 @@ export const DailyNormaContainer = styled.div`
     font-weight: 700;
     font-size: 22px;
     line-height: 1;
-    color:${props => props.theme.colors.primaryBlue};
+    color: ${(props) => props.theme.colors.primaryBlue};
     margin-bottom: 0;
     @media (min-width: 768px) {
       font-size: 24px;
